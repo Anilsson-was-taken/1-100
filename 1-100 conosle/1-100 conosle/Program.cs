4d831367dde01e9fa4a95db1112822014e040328
Alexander Nilsson <Anilsson2003@hotmail.com>
2020-11-22 16:01:23 +0100

    Första uppdatering

---
+++ 1-100 conosle/1-100 conosle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_100_conosle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int answer = random.Next(1, 101);
+            string guess;
+            int intGuess;
+            int i = 0;
+            bool correct = false;
+
+            while (correct == false)
+            {
+                i++;
+                Console.WriteLine("Guess a number between 1 and 100");
+                guess = (Console.ReadLine());
+                intGuess = int.Parse(guess);
+
+                if (intGuess < answer)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Your guess was to small");
+                }
+                else if (intGuess > answer)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Your guess was to big");
+                }
+
+            }
+        }
+    }
+}
+
